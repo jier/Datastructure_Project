e90022ef5f688da2954056350b365005a6c30055
Menno van Leeuwen <mennovleeuwen1990@gmail.com>
2014-03-06 14:05:01 +0100

    Alpha version
    This is the alpha version

--- Matrix.java
+++ Matrix.java
@@ -7,29 +7,21 @@ import java.lang.*;
 
 public abstract class Matrix{
 	
-	public abstract void remove(int elem);
-	public abstract void getGrid(int elem);
-	public abstract void checkSpot(int elem);
-	public abstract Matrix[] row();
-	public abstract Matrix[] column();
-	public abstract Matrix[][] region();
+	public void remove(int elem){
+
+	}
+	public void getGrid(int elem){
+
+	}
+	public void checkSpot(int elem){
+		
+	}
+	//public abstract Matrix[] row();
+	//public abstract Matrix[] column();
+	//public abstract Matrix[][] region();
 		
 	public boolean isStuck(){
-		boolean legal = true;
-		int [] possibilities = new int[new ArrayList();new ArrayList(); 
-		new ArrayList();new ArrayList();new ArrayList(); 
-		new ArrayList();];
-		int lengthMatrix = 9;
-		for(int i = 0;  i< lengthMatrix;i++){
-			if(possibilities[i] == possibilities[i+1]){
-				legal = false;
-			}else if (possibilities[i] == possibilities[i-1]){
-				legal = false;
-			}else{
-				legal = true;
-			}
-		}
-		return legal;
+		return false;
 	}
 		
 	/*public void time(){
--- Sudoku.java
+++ Sudoku.java
@@ -1,64 +1,23 @@
 import java.util.*;
 import java.lang.*;
-<<<<<<< HEAD
-
-public class Sudoku{
-	private Cell[][] grid;
-	private Selection[][] groups;
-	private int[][] original, generated;
-	private Set solvedCells;
-	private int count,turns,wipes, startCount;
-	private boolean stuck;
-	
-	public sudoku{
-		this(new int[9][9]);
-	}
-	
-	public sudoku(int [][] org){
-=======
 import java.io.*;
 
 public class Sudoku{
 
 	long start = 0;
 	long end = 0;
-	ArrayList<Integer> list = new ArrayList<Integer>();
-
-	private String grid;
-	//private array[][] grid;
-	//private Selection[][] groups;
-	private int[][] original, generated;
-	//private Set solvedCells;
-	private int count,turns,wipes, startCount;
-	private boolean stuck;
+	//ArrayList<Integer> list = new ArrayList<Integer>();
+	static int[] sudokuStringArray = new int[81];
 	
 	public Sudoku(){
-		this(new int[9][9]);
+		
 	}
 	
 	public Sudoku(int [][] org){
->>>>>>> ec07d99c525b1c759d2c53506761a31c316240b4
-		turns =0;
-		original = new int[9][9];
-		generated = new int[9][9];
-		for(int r =0; r< 9; r++){
-			for(int c =0; c< 9; c++){
-				if (org[r][c] != 0){
-					original[r][c] = org[r][c];
-					generated[r][c] = org[r][c];
-				}
-			}
-		}
-<<<<<<< HEAD
 		
 	}
 	
 	public void read(){
-=======
-	}
-	
-	public void read(){
-		 System.out.println("ik ga inlezen");
 		 try {
             BufferedReader file = new BufferedReader(new FileReader("single.txt"));
             //start = System.currentTimeMillis();
@@ -69,10 +28,13 @@ public class Sudoku{
                 for(int i = 0; i < str.length(); i++)
 					{
 					char c = str.charAt(i);
-				   int f = Character.digit(c,10); 
-				   System.out.printf("%s\n", c);
-				   list.add(f);
-				}				
+				   	int f = Character.digit(c,10); 
+				   	//System.out.printf("%s\n", c);
+				   	//list.add(f);
+				   	sudokuStringArray[i] = f;
+				}	
+
+				printBoard();			
             }
 
             ///end = System.currentTimeMillis();
@@ -80,7 +42,8 @@ public class Sudoku{
         } catch (IOException e) {
             e.printStackTrace();
         }
->>>>>>> ec07d99c525b1c759d2c53506761a31c316240b4
+
+
 	}
 	
 	public void benchmark(){
@@ -90,65 +53,48 @@ public class Sudoku{
 	}
 	
 	public void solve(){
+		SudokuSolver solver = new SudokuSolver(sudokuStringArray);
+
 	}
 	
 	public boolean isSolved(){
-<<<<<<< HEAD
-=======
 		return false;
->>>>>>> ec07d99c525b1c759d2c53506761a31c316240b4
 	}
 	 
 	public int getCount(){
-		return count;
+		return 5 ;
 	}
 	
-	public int[][] getOrg(){
-		return original;
+	public int getOrg(){
+		return 5;
 	}
 	
-	public int[][] getGen(){
-		return generated;
+	public int getGen(){
+		return 5;
 	}
 	
 	public int getTurn(){
-		return turns;
+		return 5;
 	}
 	
 	public int getStartCount(){
-		return startCount;
+		return 6;
 	}
-<<<<<<< HEAD
-	
-	public int[][] printMatrix(){
-		 int[][] temp = new int[9][9];
-		 for(int i =0; i < 9; i++){
-		 	for(int j= 0; j< 9; j++){
-		 		temp[i][j]= grid[i][j];
-		 	}
-		 }
-		 return temp;
-	}
-	public int [][] printResults(){
-=======
 
 	public void printBoard() {
 	
-		    for (int k = 0; k < list.size(); k++) {
+		    for (int i = 0; i < sudokuStringArray.length; i++) {
 		    	
-		        if(k%9==0 && k != 0)
-		        		System.out.println("|");
-		        	if(k%27==0)
-		                System.out.println(" -----------------------");
-		        if(k%3==0)
+		        if(i%9==0 && i != 0)
+		        	System.out.println("|");
+		        if(i%27==0)
+		            System.out.println(" -----------------------");
+		        if(i%3==0)
 		        	System.out.print("| ");
 		    	
-		    	System.out.print(list.get(k));
+		    	System.out.print(sudokuStringArray[i]);
 		    	System.out.print(' ');
-
-		            //System.out.print("\n");
-		            
-		        
+		  	        
 		    }
 		    System.out.println("|");
 		    System.out.println(" -----------------------");
@@ -164,20 +110,19 @@ public class Sudoku{
 	public int printResults(){
 
 		System.out.println("this is the end");
-		int size = list.size();
-		System.out.println(size);
+		//int size = list.size();
+		//System.out.println(size);
 		return 6;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println("Begin of program");
 	    Sudoku sudoku = new Sudoku();
-	    System.out.println(sudoku.list);
 	    sudoku.read();
-	    System.out.println(sudoku.list);
-	    sudoku.printResults();
-	    sudoku.printBoard();
->>>>>>> ec07d99c525b1c759d2c53506761a31c316240b4
+	    sudoku.solve();
+	    //System.out.println(Arrays.toString(sudokuStringArray));
+	    
+	    
 	}
 	
 	
